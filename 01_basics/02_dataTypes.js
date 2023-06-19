20c07e58c5cca219c1c4b05c0127ceb6a38bdf9c
Pritom Karmokar <71670082+PritomKarmokar@users.noreply.github.com>
2023-06-19 15:30:37 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    + 
+     3) // code readability should be high
+
+
+console.log("Pritom")
+
+let name = "pritom"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => when we declared variable but not assign any value
+// symbol => unique
+
+
+// object  
+
+
+console.log(typeof undefined) // undefined
+
+console.log(typeof null) // object
