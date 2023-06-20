48070282d137007a55006a3cb1e4f63fd2d50816
Pritom Karmokar <71670082+PritomKarmokar@users.noreply.github.com>
2023-06-20 09:58:07 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score = undefined
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber)
 
-console.log(valueInNumber)
+// console.log(valueInNumber)
 
 // "33" => 33
 // "33abc" => NaN
@@ -17,7 +17,7 @@ let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -27,4 +27,44 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+// ******************* Operations ******************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2 ** 3) // power operation
+// console.log(2 % 3)  // remainder
+
+let str1 = "hello"
+let str2 = " pritom"
+
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2) // 122 but expected value was 14
+// console.log(1 + 2 + "2") // output value 32
+
+// console.log( (3 + 4) * 5 % 3)
+
+// console.log(+true); // 1
+// console.log(+"") // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 // code reability should be high
+
+// console.table([num1, num2, num3])
+
+let gameCounter = 100
+++gameCounter
+
+console.log(gameCounter)
+
+// postfix and prefix operator : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
