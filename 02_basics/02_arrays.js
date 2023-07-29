b48e3611e888838fc233c6ba63372e2db14dd19f
Pritom Karmokar <71670082+PritomKarmokar@users.noreply.github.com>
2023-07-29 15:19:56 +0000

    array added

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,28 +1,36 @@
-const marvel_heros = ["thor", "Ironman", "Spiderman"]
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+// Pushing 'dc_heros' into 'marvel_heros'
+// marvel_heros.push(dc_heros) // Creates an array within another array
 
 // console.log(marvel_heros)
 
-// console.log(marvel_heros[3][2])
+// console.log(marvel_heros[3])
 
-// const allHeros = marvel_heros.concat(dc_heros)
+// Merging two arrays properly using 'concat'
+const allHeros = marvel_heros.concat(dc_heros)
+
 // console.log(allHeros)
 
-const all_new_heros = [...marvel_heros, ...dc_heros] // soread operator
+// Merging two arrays using 'spread operator'
+const all_new_heros = [...marvel_heros, ...dc_heros] 
 
 // console.log(all_new_heros)
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
+const real_another_array = another_array.flat(Infinity) // returns a new flatten array
 
 console.log(real_another_array)
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "pritom"})) // interesting
+console.log(Array.isArray("Hitesh")) // checking if the "Hitesh" is an array or not
+
+console.log(Array.from("Hitesh")) // converting "Hitesh" into an array
+
+// interesting
+console.log(Array.from({name: "Hitesh"})) // It will return 'null' since we don't specify what values to convert into an array 'key' or 'value'
 
 let score1 = 100
 let score2 = 200
